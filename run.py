00c404665604b40c756a5c028e491ab354cc4c5e
alperkan1 <seanhack@gmail.com>
2021-09-17 11:10:03 +0000

    crate the board

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+FIRST THE 2 BOARDS CREAT THE BOARDS
+"""
+MY_BOARD = [[" "] * 8 for x in range (8)]
+HIS_BOARD = [[" "] * 8 for x in range (8)]

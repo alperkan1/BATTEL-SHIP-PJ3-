25e408d0f2b17f3eca3905236ad06f52b7e128fe
alperkan1 <seanhack@gmail.com>
2021-09-17 11:14:29 +0000

    allocation of board numbers

--- run.py
+++ run.py
@@ -3,3 +3,12 @@ FIRST THE 2 BOARDS CREAT THE BOARDS
 """
 MY_BOARD = [[" "] * 8 for x in range (8)]
 HIS_BOARD = [[" "] * 8 for x in range (8)]
+
+"""
+converting the numbers to letter
+"""
+numbers = {"a":0, "b":1, "c":3, "d":4, "e":5, "f":5, "g":6, "h":7, "k":8}
+
+"""
+printing the boards
+"""

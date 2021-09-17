a5bd2166009ca44547b8f480b2d44b0e40e29efa
alperkan1 <seanhack@gmail.com>
2021-09-17 14:43:34 +0000

    board is now working

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from random import randint
 
-
+board = []
 """
 FIRST THE 2 BOARDS CREAT THE BOARDS
 """
@@ -20,12 +20,12 @@ def create_board(board):
     print("---------------")
     rows = 1
     for row in board:
-        print("%d|%s|" % (rows, "/" . join(row)))
+        print("%d|%s|" % (rows, "/". join(row)))
         rows += 1
 """
 Creating the ships to sink for the game 
 """        
-def ships():
+def ships(board):
     for ship in range(5):
         ship_verticle, ship_horizontal = randint(0,7), randint(0,7)
         while board[ship_verticle][ship_horizontal] == "X":
@@ -33,15 +33,32 @@ def ships():
         board[ship_verticle][ship_horizontal] = "X"
 
 """
-To define the location of the ships in the game 
+To define hiting the location of the ships in the game 
 """
 
 def location_ship():
     row = input("Enter number between 1-8")
-    while row not in "12345678":
+    while row not in "12345678" or ValueError:
         print("Wrong input number out side of 1-8")
         row = input("Enter number between 1-8")
     column = input("Enter a letter between a-h").lower()
-    while column not in "abcdefgh":
+    while column not in "abcdefgh" or ValueError:
         print("Wrong input letter should be between a-h").lower()
         input("Enter a letter between a-h").lower()
+    return int(row) - 1, numbers[column]
+
+"""
+what happens when you hit a ship
+"""
+
+def ship_hit(board):
+    count = 0
+    for row in board:
+        for column in row:
+            if column == "X":
+                count += 1
+    return count
+
+ships(HIS_BOARD)
+turns = 12
+create_board(HIS_BOARD)

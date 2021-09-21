98a97a74a00852ced1f5cd3d48b013f69cce2ed6
alperkan1 <seanhack@gmail.com>
2021-09-21 08:10:24 +0000

    fixed all bugs

--- run.py
+++ run.py
@@ -4,17 +4,19 @@ board = []
 """
 FIRST THE 2 BOARDS CREAT THE BOARDS
 """
-MY_BOARD = [[" "] * 8 for x in range (8)]
-HIS_BOARD = [[" "] * 8 for x in range (8)]
+MY_BOARD = [[" "] * 8 for x in range(8)]
+HIS_BOARD = [[" "] * 8 for x in range(8)]
 
 """
 converting the numbers to letter
 """
-numbers = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7}
+numbers = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
 
 """
 printing the boards
 """
+
+
 def create_board(board):
     print('  a b c d e f g h')
     print("  ---------------")
@@ -22,19 +24,23 @@ def create_board(board):
     for row in board:
         print("%d|%s|" % (rows, "/". join(row)))
         rows += 1
+
 """
-Creating the ships to sink for the game 
-"""        
+Creating the ships to sink for the game
+"""
+
+
 def ships(board):
     for ship in range(5):
-        ship_verticle, ship_horizontal = randint(0,7), randint(0,7)
+        ship_verticle, ship_horizontal = randint(0, 7), randint(0, 7)
         while board[ship_verticle][ship_horizontal] == "X":
-            ship_verticle, ship_horizontal = randint(0,7), randint(0,7)
+            ship_verticle, ship_horizontal = randint(0, 7), randint(0, 7)
         board[ship_verticle][ship_horizontal] = "X"
 
 """
-To define hiting the location of the ships in the game 
+To define hiting the location of the ships in the game
 """
+
 
 def location_ship():
     row = input("Enter number between: 1-8 \n")
@@ -50,6 +56,7 @@ def location_ship():
 """
 what happens when you hit a ship
 """
+
 
 def ship_hit(board):
     hit = 0
@@ -69,7 +76,7 @@ while turns > 0:
     row, column = location_ship()
     if HIS_BOARD[row][column] == "-":
         print("Thats the same entry try again")
-    elif MY_BOARD[row][column] =="X":
+    elif MY_BOARD[row][column] == "X":
         print("HIT!!! you hit my ship :( ")
         HIS_BOARD[row][column] = "X"
         turns -= 1
@@ -85,5 +92,5 @@ while turns > 0:
     if turns == 0:
         print("game over restart")
         break
-# X means you fit the ship 
+# X means you fit the ship
 # - means a miss
